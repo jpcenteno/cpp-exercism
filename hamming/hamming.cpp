72a70749087d0cfc6a9403b4523e3978b33c2cdd
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-15 02:30:15 -0300

    hamming: download files and make it compile.

---
+++ hamming/hamming.cpp
@@ -0,0 +1,11 @@
+#include "hamming.h"
+
+namespace hamming {
+
+    unsigned int compute(std::string s1, std::string s2) {
+        s1.size();
+        s2.size();
+        return 0;
+    }
+
+}  // namespace hamming
