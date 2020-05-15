46539c459514fa8cbb6cd03c49a229ab57e176fe
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-15 04:28:11 -0300

    nucleotide_count: make it compile.

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -2,4 +2,16 @@
 
 namespace nucleotide_count {
 
+    counter::counter(const std::string) {
+
+    }
+
+    const std::map<char, int>& counter::nucleotide_counts() const {
+        return _nucleotideCounts;
+    }
+
+    unsigned int counter::count(char) const {
+        return 0;
+    }
+
 }  // namespace nucleotide_count
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -1,7 +1,25 @@
 #if !defined(NUCLEOTIDE_COUNT_H)
 #define NUCLEOTIDE_COUNT_H
 
+#include <map>
+#include <string>
+
 namespace nucleotide_count {
+
+    class counter {
+    public:
+
+        counter(const std::string);
+
+        const std::map<char, int>& nucleotide_counts() const;
+
+        unsigned int count(char) const;
+
+    private:
+
+        std::map<char, int> _nucleotideCounts;
+
+    };
 
 }  // namespace nucleotide_count
 
