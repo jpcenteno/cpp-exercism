e615d4238939f2bd0068877c0edecdab4160db18
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-15 02:41:46 -0300

    hamming: make tests pass.

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,11 +1,24 @@
 #include "hamming.h"
 
+#include <algorithm>
+
 namespace hamming {
 
     unsigned int compute(std::string s1, std::string s2) {
-        s1.size();
-        s2.size();
-        return 0;
+
+        if (s1.size() != s2.size()) {
+            throw std::domain_error("s1 and s2 have different sizes.");
+        }
+
+        unsigned int count = 0;
+
+        for (size_t i = 0; i < s1.size(); ++i) {
+            if (s1[i] != s2[i]) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 }  // namespace hamming
