7e6905f63907789553b900b6e3b62f22cd758c40
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-15 12:58:43 -0300

    grade_school: make it compile

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -2,4 +2,9 @@
 
 namespace grade_school {
 
+    map<int, vector<string>> school::roster() const {
+        map<int, vector<string>> map;
+        return map;
+    }
+
 }  // namespace grade_school
--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,7 +1,22 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 namespace grade_school {
+
+    class school {
+    public:
+
+        map<int, vector<string>> roster() const;
+
+
+    private:
+    };
 
 }  // namespace grade_school
 
