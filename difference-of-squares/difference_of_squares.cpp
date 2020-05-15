3e6911bb595a14bfe0f1feebecc832a3985670a9
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-15 03:09:42 -0300

    difference-of-squares: implement solution.

--- difference-of-squares/difference_of_squares.cpp
+++ difference-of-squares/difference_of_squares.cpp
@@ -2,4 +2,18 @@
 
 namespace difference_of_squares {
 
+    unsigned int square_of_sum(unsigned int n) {
+        auto sum = (n * (n + 1)) / 2;
+        return sum * sum;
+    }
+
+    unsigned int sum_of_squares(unsigned int n) {
+        auto sum = n * (n + 1) * (2 * n + 1) / 6;
+        return sum;
+    }
+
+    unsigned int difference(unsigned int n) {
+        return square_of_sum(n) - sum_of_squares(n);
+    }
+
 }  // namespace difference_of_squares
--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -3,6 +3,10 @@
 
 namespace difference_of_squares {
 
+    unsigned int square_of_sum(unsigned int);
+    unsigned int sum_of_squares(unsigned int);
+    unsigned int difference(unsigned int);
+
 }  // namespace difference_of_squares
 
 #endif // DIFFERENCE_OF_SQUARES_H
