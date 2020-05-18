942581d31d600746d98f572f7e49b61ae358a6dd
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-18 15:49:58 -0300

    rna_transcription: pass the char2char tests.

--- rna-transcription/rna_transcription.cpp
+++ rna-transcription/rna_transcription.cpp
@@ -2,7 +2,16 @@
 
 namespace rna_transcription {
 
-    char to_rna(const char) {
+    char to_rna(const char nucleotide) {
+        if (nucleotide == 'C') {
+            return 'G';
+        } else if (nucleotide == 'G') {
+            return 'C';
+        } else if (nucleotide == 'A') {
+            return 'U';
+        } else if (nucleotide == 'T') {
+            return 'A';
+        }
         return 0;
     }
 
