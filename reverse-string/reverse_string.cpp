5cdea65c34cff20d71f33846518cff83415f2fb4
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-18 21:31:58 -0300

    reverse_string: Add solution.

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -3,7 +3,16 @@
 namespace reverse_string {
 
     std::string reverse_string(const std::string& input_string) {
-        return input_string;
+
+        // Copy `input_string` into a mutable string and reverse it.
+        std::string out(input_string);
+        std::string::size_type n = out.size();
+        for (std::string::size_type i = 0; i < n / 2; ++i) {
+            std::swap(out[i], out[n - i - 1]);
+        }
+
+        return out;
+
     }
 
 }  // namespace reverse_string
