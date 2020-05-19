3ffec3386ee3dd7d1a162421710a3d3856edca9a
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-18 23:45:16 -0300

    two_fer: implement solution.

--- two-fer/two_fer.cpp
+++ two-fer/two_fer.cpp
@@ -1,7 +1,22 @@
 #include "two_fer.h"
 
+#include <sstream>
+
 namespace two_fer
 {
+
+    std::string two_fer() {
+        return "One for you, one for me.";
+    }
+
+    std::string two_fer(const std::string& name) {
+
+        // Construct the string using a ostringstream:
+        std::ostringstream ss;
+        ss << "One for " << name << ", one for me.";
+        return ss.str();
+
+    }
 
 } // namespace two_fer
 
