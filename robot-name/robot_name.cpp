a7bee9e98d9bd35f36d239d66e2545417352b3b9
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-22 19:27:41 -0300

    robot_name: make tests pass

--- robot-name/robot_name.cpp
+++ robot-name/robot_name.cpp
@@ -1,5 +1,38 @@
 #include "robot_name.h"
 
+#include <random>
+#include <string>
+
 namespace robot_name {
+
+    std::mt19937 gen(42); // PRNG, Always with same seed, this is an exercise.
+    std::uniform_int_distribution<> letter_distribution(0, 'Z' - 'A');
+    std::uniform_int_distribution<> digit_distribution(0, 9);
+
+    std::string generate_name() {
+
+        std::string name;
+
+        name.push_back('A' + letter_distribution(gen));
+        name.push_back('A' + letter_distribution(gen));
+        name.push_back('0' + digit_distribution(gen));
+        name.push_back('0' + digit_distribution(gen));
+        name.push_back('0' + digit_distribution(gen));
+
+        return name;
+
+    }
+
+    robot::robot() {
+        _name = generate_name();
+    }
+
+    std::string robot::name() const {
+        return _name;
+    }
+
+    void robot::reset() {
+        _name = generate_name();
+    }
 
 }  // namespace robot_name
--- robot-name/robot_name.h
+++ robot-name/robot_name.h
@@ -1,7 +1,25 @@
 #if !defined(ROBOT_NAME_H)
 #define ROBOT_NAME_H
 
+#include <string>
+
 namespace robot_name {
+
+    class robot {
+
+    public:
+
+        robot();
+
+        std::string name() const;
+
+        void reset();
+
+    private:
+
+        std::string _name;
+
+    };
 
 }  // namespace robot_name
 
