d127ce49a12c783bfbdb5aa207496caa346ada32
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-22 16:32:29 -0300

    collatz_conjecture: make tests pass

--- collatz-conjecture/collatz_conjecture.cpp
+++ collatz-conjecture/collatz_conjecture.cpp
@@ -1,9 +1,28 @@
 #include "collatz_conjecture.h"
 
+#include <stdexcept>
+
 namespace collatz_conjecture {
 
     std::int64_t steps(std::int64_t n) {
-        return n;
+
+        if (n < 1) {
+            throw std::domain_error("n must be possitive");
+        }
+
+        std::int64_t i = 0;
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+
+            i++;
+        }
+
+        return i;
     }
 
 }  // namespace collatz_conjecture
