0e07d1fc2af6da67325722330ae7cb0580f4ed0c
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-22 16:24:32 -0300

    collatz_conjecture: make it compile

--- collatz-conjecture/collatz_conjecture.cpp
+++ collatz-conjecture/collatz_conjecture.cpp
@@ -2,4 +2,8 @@
 
 namespace collatz_conjecture {
 
+    std::int64_t steps(std::int64_t n) {
+        return n;
+    }
+
 }  // namespace collatz_conjecture
--- collatz-conjecture/collatz_conjecture.h
+++ collatz-conjecture/collatz_conjecture.h
@@ -1,7 +1,11 @@
 #if !defined(COLLATZ_CONJECTURE_H)
 #define COLLATZ_CONJECTURE_H
 
+#include <cstdint>
+
 namespace collatz_conjecture {
+
+    std::int64_t steps(std::int64_t n);
 
 }  // namespace collatz_conjecture
 
