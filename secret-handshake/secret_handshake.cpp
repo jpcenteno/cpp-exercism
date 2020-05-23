0ef9148da8f28c455ec4eddcf629a418ce232263
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-23 17:04:21 -0300

    secret_handshake: complete exercise.

--- secret-handshake/secret_handshake.cpp
+++ secret-handshake/secret_handshake.cpp
@@ -1,5 +1,41 @@
 #include "secret_handshake.h"
 
+#include <algorithm>
+
 namespace secret_handshake {
+
+    constexpr std::uint64_t mask_wink         = 1 << 0;
+    constexpr std::uint64_t mask_double_blink = 1 << 1;
+    constexpr std::uint64_t mask_close_eyes   = 1 << 2;
+    constexpr std::uint64_t mask_jump         = 1 << 3;
+    constexpr std::uint64_t mask_reverse      = 1 << 4;
+
+    std::vector<std::string> commands(std::uint64_t code) {
+
+        std::vector<std::string> sequence;
+
+        if (code & mask_wink) {
+            sequence.push_back("wink");
+        }
+
+        if (code & mask_double_blink) {
+            sequence.push_back("double blink");
+        }
+
+        if (code & mask_close_eyes) {
+            sequence.push_back("close your eyes");
+        }
+
+        if (code & mask_jump) {
+            sequence.push_back("jump");
+        }
+
+        if (code & mask_reverse) {
+            std::reverse(sequence.begin(), sequence.end());
+        }
+
+        return sequence;
+
+    }
 
 }  // namespace secret_handshake
--- secret-handshake/secret_handshake.h
+++ secret-handshake/secret_handshake.h
@@ -1,7 +1,12 @@
 #if !defined(SECRET_HANDSHAKE_H)
 #define SECRET_HANDSHAKE_H
 
+#include <vector>
+#include <string>
+
 namespace secret_handshake {
+
+    std::vector<std::string> commands(std::uint64_t code);
 
 }  // namespace secret_handshake
 
