d6cff9cc5905dbcac85770fa9527e44d8a3b02c7
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-31 10:42:36 -0300

    space_age: make tests pass for every other planet

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -4,6 +4,14 @@ namespace space_age {
 
     constexpr double seconds_in_earth_year = 31557600.0;
 
+    constexpr double orbital_period_mercury = 0.2408467;
+    constexpr double orbital_period_venus = 0.61519726;
+    constexpr double orbital_period_mars = 1.8808158;
+    constexpr double orbital_period_jupiter = 11.862615;
+    constexpr double orbital_period_saturn = 29.447498;
+    constexpr double orbital_period_uranus = 84.016846;
+    constexpr double orbital_period_neptune = 164.79132;
+
     // Constructors:
 
     space_age::space_age(std::uint64_t earth_age_in_seconds) 
@@ -19,6 +27,34 @@ namespace space_age {
 
     double space_age::on_earth() const {
         return _earth_age_in_seconds / seconds_in_earth_year;
+    }
+
+    double space_age::on_mercury() const {
+        return on_earth() / orbital_period_mercury;
+    }
+
+    double space_age::on_venus() const {
+        return on_earth() / orbital_period_venus;
+    }
+
+    double space_age::on_mars() const {
+        return on_earth() / orbital_period_mars;
+    }
+
+    double space_age::on_jupiter() const {
+        return on_earth() / orbital_period_jupiter;
+    }
+
+    double space_age::on_saturn() const {
+        return on_earth() / orbital_period_saturn;
+    }
+
+    double space_age::on_uranus() const {
+        return on_earth() / orbital_period_uranus;
+    }
+
+    double space_age::on_neptune() const {
+        return on_earth() / orbital_period_neptune;
     }
 
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -20,6 +20,13 @@ namespace space_age {
         // Age on planets:
 
         double on_earth() const;
+        double on_mercury() const;
+        double on_venus() const;
+        double on_mars() const;
+        double on_jupiter() const;
+        double on_saturn() const;
+        double on_uranus() const;
+        double on_neptune() const;
 
     private:
 
