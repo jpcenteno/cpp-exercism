abdbdc7303695b1b0233744d3185b823741a3572
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2020-05-31 10:20:12 -0300

    space_age: solve first test

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -2,4 +2,11 @@
 
 namespace space_age {
 
+    space_age::space_age(std::uint64_t earth_age_in_seconds) 
+        : _earth_age_in_seconds(earth_age_in_seconds) {}
+
+    std::uint64_t space_age::seconds() const {
+        return _earth_age_in_seconds;
+    }
+
 }  // namespace space_age
--- space-age/space_age.h
+++ space-age/space_age.h
@@ -1,7 +1,22 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
 
+#include <cstdint>
+
 namespace space_age {
+
+    class space_age {
+
+    public:
+        space_age(std::uint64_t earth_age_in_seconds);
+
+        std::uint64_t seconds() const;
+
+    private:
+
+        std::uint64_t _earth_age_in_seconds;
+
+    };
 
 }  // namespace space_age
 
